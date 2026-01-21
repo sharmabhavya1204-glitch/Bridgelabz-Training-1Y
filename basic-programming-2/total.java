ce164aefe06bc5ad915a2565d92b66a72bccb2ec
bhavya180607 <sharmabhavya1204@gmail.com>
2026-01-21 11:46:27 +0530

    [Bhavya] total.java

---
+++ basic-programming-2/total.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class total{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+System.out.println("unit price of item");
+double a=sc.nextDouble();
+System.out.println("quantity of item");
+double b=sc.nextDouble();
+double c=a*b;
+System.out.println("total"+c);
+}}
