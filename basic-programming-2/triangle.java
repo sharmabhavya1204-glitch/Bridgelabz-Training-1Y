c57097fde29ea19400069fe9df8cd35b9075a328
bhavya180607 <sharmabhavya1204@gmail.com>
2026-01-21 11:39:59 +0530

    [Bhavya] triangle.java added

---
+++ basic-programming-2/triangle.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class traingle{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+System.out.println("enter height");
+double a=sc.nextDouble();
+System.out.println("enter base");
+double b=sc.nextDouble();
+double c=.5*a*b;
+System.out.println("area is"+c);
+}}
