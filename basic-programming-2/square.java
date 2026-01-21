34ff18653e0ba64c7e495bfad9a164b11f9b3c8c
bhavya180607 <sharmabhavya1204@gmail.com>
2026-01-21 11:23:44 +0530

    [Bhavya] basic-programming-1 added

---
+++ basic-programming-2/square.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class square{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+System.out.println("enter perimeter of square");
+double a=sc.nextDouble();
+double b=a/4;
+System.out.println("side of square is"+b);
+}}
